a223d94a78c188ff89f4ab461ab76c0ce42f0fda
Robert Shields <robert.shieldsiv@gmail.com>
2020-10-08 20:20:55 -0400

    migrated backend users and added routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_022257) do
+ActiveRecord::Schema.define(version: 2020_10_09_001013) do
 
   create_table "photos", force: :cascade do |t|
     t.boolean "isFavorite"
     t.string "url"
     t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
